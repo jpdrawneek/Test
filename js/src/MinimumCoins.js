8d25efb4ff8c5740ee25afdc55d0fd60638d081c
jpd4nt <jdrawneek@nationaltheatre.org.uk>
2014-06-13 21:36:40 +0100

    refactor generic code out into its own method.

--- js/src/MinimumCoins.js
+++ js/src/MinimumCoins.js
@@ -4,16 +4,26 @@
  */
 
 function MinimumCoins() {
+    var values = [];
+    var label = [];
 }
 MinimumCoins.prototype.sterling = function(pence) {
-    var values = [200, 100, 50, 20, 2, 1];
-    var label = ['£2', '£1', '50p', '20p', '2p', '1p'];
+    values = [200, 100, 50, 20, 2, 1];
+    label = ['£2', '£1', '50p', '20p', '2p', '1p'];
+    return this._process(pence, values, label);
+};
+/**
+ * Private method to do the actual calculating.
+ *
+ * @param integer pence
+ * @returns {MinimumCoins.prototype._process.output}
+ */
+MinimumCoins.prototype._process = function(pence, values, labels) {
     var output = {};
     values.forEach(function(element, index, array) {
         var count = pence / element;
         pence = pence % element;
-        output[label[index]] = parseInt(count);
+        output[labels[index]] = parseInt(count);
     });
-        
     return output;
 };
