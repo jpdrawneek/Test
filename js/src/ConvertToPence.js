cc5149984545662a32617daa2c6ac92fc556be3f
jpd4nt <jdrawneek@nationaltheatre.org.uk>
2014-06-13 23:35:00 +0100

    Refactor so that the conversion rules can be easily extended and other currencies to be added.

--- js/src/ConvertToPence.js
+++ js/src/ConvertToPence.js
@@ -6,40 +6,69 @@
 function ConvertToPence() {
     this.error = 0;
     this.errorsList = [];
+    this.list = [];
 }
 /**
  * Convert the entered Sterling value to Sterling pence.
  *
- * @param mixed input
+ * @param {mixed} input
  *   Either string or an integer
  *
- * @returns Number
+ * @returns {Number}
  *   0 on failure or 0 inputed. Otherwise the pence value of the input.
  */
 ConvertToPence.prototype.sterling = function(input) {
+    list = ['stripSterlingSymbols', 'checkForExtraCharacters', 'stripDecimal', 'checkNumber'];
+    return this._process(input, list);
+};
+/**
+ * Private meothod that preforms the conversion.
+ *
+ * @param {mixed} input
+ *   This is the input value to be converted.
+ * @param {Array} list
+ *   List of rules to be applied as part of the conversion process.
+ *   
+ * @returns {Number}
+ *   The input converted to a number. If error it returns 0.
+ */
+ConvertToPence.prototype._process = function(input, list) {
     var output;
     if ((output = this.checkNumber(input)) !== false) {
         return output;
     } else {
-        tmp = this.stripSterlingSymbols(input);
-        if (!this.checkForExtraCharacters(tmp)) {
-            tmp = this.stripDecimal(tmp);
-            if ((output = this.checkNumber(tmp)) !== false) {
-                return output;
+        var ob = this;
+        list.forEach(function(element) {
+            input = ob[element](input);
+        });
+        if (input === false) {
+            this.error = 1;
+            input = 0;
+            if (this.errorsList.length === 0) {
+                this.errorsList.push('No value entered');
             }
-            this.errorsList.push('No value entered');
         }
-        this.error = 1;
-        return 0;
+        console.log(input);
+        return input;
     }
-};
+}
 
 ConvertToPence.prototype.clearErrors = function() {
     this.error = 0;
     this.errorsList = [];
 };
-
+/**
+ * 
+ * @param {mixed} input
+ *  This is the input value to be converted.
+ *
+ * @returns {Number|Boolean}
+ *   Returns false on failure or the number on success.
+ */
 ConvertToPence.prototype.checkNumber = function(input) {
+    if (input === false) {
+        return false;
+    }
     var regex = /^\d+$/i
     if (typeof input==='number' && (input%1)===0) {
         return input;
@@ -53,25 +82,55 @@ ConvertToPence.prototype.checkNumber = function(input) {
 ConvertToPence.prototype.parseInt = function(input) {
     return parseInt(input);
 };
-
+/**
+ * 
+ * @param {mixed} input
+ *  This is the input value to be converted.
+ *
+ * @returns {Number|Boolean}
+ *   Returns false on failure or the number on success.
+ */
 ConvertToPence.prototype.stripSterlingSymbols = function(input) {
+    if (input === false) {
+        return false;
+    }
     var tmp;
     tmp = input.replace(/p\s*$/i, '');
     tmp = tmp.replace(/^\s*£/i, '');
     return tmp;
 };
-
+/**
+ * 
+ * @param {mixed} input
+ *  This is the input value to be converted.
+ *
+ * @returns {Number|Boolean}
+ *   Returns false on failure or the number on success.
+ */
 ConvertToPence.prototype.checkForExtraCharacters = function(input) {
+    if (input === false) {
+        return false;
+    }
     var count = input.match(/[^\.0-9]/g);
     if (count === null) {
-        return false;
+        return input;
     } else {
         this.errorsList.push('Invalid characters entered');
-        return true;
+        return false;
     }
 };
-
+/**
+ * 
+ * @param {mixed} input
+ *  This is the input value to be converted.
+ *
+ * @returns {Number|Boolean}
+ *   Returns false on failure or the number on success.
+ */
 ConvertToPence.prototype.stripDecimal = function(input) {
+    if (input === false) {
+        return false;
+    }
     var tmp;
     var count = input.match(/\./g);
     if (count === null) {
